44296400b676fe4158bcdb9c681118032ae7a247
julietwamboi <wamboijuliet02@gmail.com>
2026-01-26 13:58:08 +0300

    basichtml

---
+++ chrome.js
@@ -0,0 +1,10 @@
+  function myfunction(){
+            document.getElementById("mybtn").style.backgroundColor="pink"
+        }
+        function myalert(){
+            
+            alert("hello goodmorning")
+        }
+        function abcd(){
+            document.getElementById("mypara").style.backgroundColor="red"
+        }
